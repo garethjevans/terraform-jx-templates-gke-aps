f20c9e000721a63af2b89350e4f9ebe4d8cd7ef1
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-07-11 19:33:39 +0100

    fix: removed provider config

--- main.tf
+++ main.tf
@@ -195,15 +195,6 @@ resource "google_kms_crypto_key" "vault-crypto-key" {
   }
 }
 
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.jx-cluster.endpoint}"
-  username               = "${google_container_cluster.jx-cluster.master_auth.0.username}"
-  password               = "${google_container_cluster.jx-cluster.master_auth.0.password}"
-  client_certificate     = "${base64decode(google_container_cluster.jx-cluster.master_auth.0.client_certificate)}"
-  client_key             = "${base64decode(google_container_cluster.jx-cluster.master_auth.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(google_container_cluster.jx-cluster.master_auth.0.cluster_ca_certificate)}"
-}
-
 # Render the k8s YAML file
 data "template_file" "service" {
   template = "${file("${path.module}/service.yaml")}"
